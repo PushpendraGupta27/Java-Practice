bb857dc14ec6b70cc76d36a2440324c406b23f1d
Pushpendra <pushpendragupta032@gmail.com>
2025-04-20 20:59:39 +0530

    Programs

--- src/Arrays_Basic_Operations.java
+++ src/Arrays_Basic_Operations.java
@@ -1,68 +1,91 @@
+import java.util.*;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arrays_Basic_Operations {
     private static final int[] array = {4, 6, 9, 5, 2, 3};
+    private static final int[] arr = {1, 2, 2, 3, 4, 4, 5};
+    private static final int[] newArr = new int[array.length];
     static Scanner scn = new Scanner(System.in);
 
     private static void questions() {
         int sum = 0;
         int min = array[0];
         int max = array[0];
-        boolean isSorted = false;
+        boolean isSorted = true;
 
-        System.out.print("Enter a element which you want to search: ");
+        System.out.print("Enter an element you want to search: ");
         int num = scn.nextInt();
         int index = -1;
+        int temp;
+        int len = array.length;
+        Set<Integer> set = new HashSet<>();
 
-        for (int i = 0; i <= array.length - 1; i++) {
-            // Find the sum of all elements in an array.
-            sum = array[i] + sum;
 
-            // Find the maximum and minimum elements in an array.
+        for (int i = 0; i < len; i++) {
+            // Sum of all elements
+            sum += array[i];
+
+            // Max and min
             if (array[i] < min) {
                 min = array[i];
-            } else if (array[i] > max) {
+            }
+            if (array[i] > max) {
                 max = array[i];
             }
 
-            // Search for an element in an array (linear search).
-            if (array[i] == num) {
+            // Linear search
+            if (array[i] == num && index == -1) {
                 index = i;
-                break;
             }
 
-            isSorted = array[i] < array[i + 1];
+            // Check if array is sorted
+            if (i < len - 1 && array[i] > array[i + 1]) {
+                isSorted = false;
+            }
+
+            // Copy elements of one array into another array.
+            newArr[i] = array[i];
         }
+
         if (index != -1) {
-            System.out.println("Given element is in array index of: " + index);
+            System.out.println("Element found at index: " + index);
         } else {
-            System.out.println("Given element is not found in array.");
-        }       
+            System.out.println("Element not found in array.");
+        }
 
-        // Check if an array is sorted in ascending order.
         if (isSorted) {
-            System.out.println("Array is sorted in ascending order");
-        } else {                                                               
-            System.out.println("array is not sorted in ascending order");
-        }                                                                      
-        
-        System.out.println("Sum of all elements in an array is " + sum);
+            System.out.println("Array is sorted in ascending order.");
+        } else {
+            System.out.println("Array is not sorted in ascending order.");
+        }
 
-        // Find the average of all elements in an array.
-        int avg = sum / array.length;
-        System.out.println("Average of all elements in an array is " + avg);
-        System.out.println("maximum and minimum elements are " + max + "," + min);
+        System.out.println("Sum of elements: " + sum);
+        System.out.println("Average of elements: " + (sum / len));
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
 
-        // Reverse an array.
-        int temp;
-        int len = array.length;
-        for (int i = 0; i <= len / 2; i++) {
+        // Reverse the array
+        for (int i = 0; i < len / 2; i++) {
             temp = array[i];
             array[i] = array[len - 1 - i];
             array[len - 1 - i] = temp;
         }
-        System.out.println(Arrays.toString(array));
+
+        System.out.println("Reversed array: " + Arrays.toString(array));
+
+        System.out.println("copy array: " + Arrays.toString(newArr));
+
+        // Remove duplicates from an array.
+        for (int i = 0; i < arr.length - 1; i++) {
+            set.add(arr[i]);
+        }
+
+        System.out.println("Removed duplicates array: " + set);
+
+        // Optional: Close the scanner
+        scn.close();
     }
 
     public static void main(String[] args) {
