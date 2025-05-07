c200810c07b83807ab62683e3c32727da7095f6b
Pushpendra <pushpendragupta032@gmail.com>
2025-05-07 19:01:11 +0530

    code389 optimize way

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,16 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode {
     public static void main(String[] args) {
         code389();
     }
 
-    private static void code389() {
-
+    private static char code389() {
         String t = "abedk";
         String s = "abkd";
 
         char[] sc = s.toCharArray();
         char[] tc = t.toCharArray();
-        // brute force
+
+        // 1. Brute force
         for (char c : tc) {
             boolean isFound = false;
             for (char value : sc) {
@@ -20,8 +23,33 @@ public class LeetCode {
                 }
             }
             if (!isFound) {
-                System.out.println("Result " + c);
+                System.out.println("Brute Force Result: " + c);
             }
         }
+
+        // 2. ASCII sum
+        int sumtc = 0, sumsc = 0;
+        for (char c : tc) sumtc += c;
+        for (char c : sc) sumsc += c;
+        System.out.println("ASCII Sum Result: " + (char) (sumtc - sumsc));
+
+        // 3. XOR
+        int xor = 0;
+        for (char c : sc) xor ^= c;
+        for (char c : tc) xor ^= c;
+        System.out.println("XOR Result: " + (char) xor);
+
+        // 4. HashMap
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : sc) map.put(c, map.getOrDefault(c, 0) + 1);
+        for (char c : tc) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) < 0) {
+                System.out.println("HashMap Result: " + c);
+                return c;
+            }
+        }
+
+        return ' ';
     }
 }
